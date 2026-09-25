287155ee71e850abcc5b742d41f94deb0703432d
habiba zainab <habibazainabali@gmail.com>
2026-09-25 13:16:23 -0700

    Set 2 header

---
+++ 02_list_methods_slicing.py
@@ -0,0 +1,9 @@
+"""
+
+===========================================================
+   LECTURE 03 - SET 02 : LIST METHODS & SLICING
+   Topics : List Methods & Slicing(Positive, Negative, Step)
+   Total Questions :  
+============================================================
+
+"""
