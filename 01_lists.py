e5bdfe48e4714233b73af05ce4758697eaf593e9
habiba zainab <habibazainabali@gmail.com>
2026-09-25 11:27:06 -0700

    Solved program of creating list

--- 01_lists.py
+++ 01_lists.py
@@ -22,4 +22,14 @@
 
 print("\n--- Q1: Creating Lists ---")
 
+empty = []
+integers = [1, 2, 3, 4, 5]
+fruits = ['apple', 'banana', 'mango']
+mixed = [1, 'hello', 25.5, True]
 
+print("Empty:", empty, "(Length:", len(empty), ")")
+print("Integers:", integers, "(Length:", len(integers), ")")
+print("Fruits:", fruits, "(Length:", len(fruits), ")")
+print("Mixed:", mixed, "(Length:", len(mixed), ")")
+
+# ----------------------------------------------------------
