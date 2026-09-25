0d3a9f2689bdbb4337a8fe319e09bfb3492031f1
habiba zainab <habibazainabali@gmail.com>
2026-09-25 11:17:17 -0700

    Q1 of list creations & types

--- 01_lists.py
+++ 01_lists.py
@@ -11,3 +11,11 @@
 # ==========================================================
 # PART A:   List Creation & Types
 # ==========================================================
+
+# Q1: Create and display different types of lists
+#    Create:
+#    - Empty list
+#    - List of 5 integers
+#    - Lists of 3 strings (fruits)
+#    - Lists with mixed data types
+#    Print each with its length
