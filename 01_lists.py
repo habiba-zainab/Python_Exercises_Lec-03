dadc679a169e0a48056a3bbb68c57bc0fe627174
habiba zainab <habibazainabali@gmail.com>
2026-09-25 12:39:43 -0700

    list1 & list2 concatenated

--- 01_lists.py
+++ 01_lists.py
@@ -143,3 +143,11 @@ print("After middle change:", grades)
 #   - Create list3 combining list1 + list2 + list1
 
 print("\n--- Q6: Concatenation and repition ---")
+
+list1 = [1, 2, 3]
+list2 = [4, 5, 6]
+
+print("list1:", list1)
+print("list2:", list2)
+print("Concatenated:", list1 + list2)
+print("list1 * 3:", list1 * 3)
