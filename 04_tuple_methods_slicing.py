9f399637a5b2dc66490c6e84030126e0189e3cde
habiba zainab <habibazainabali@gmail.com>
2026-09-26 10:31:33 -0700

    solved program of count() method

--- 04_tuple_methods_slicing.py
+++ 04_tuple_methods_slicing.py
@@ -18,3 +18,14 @@
 #    - How many times 2 appears
 #    - How many times 5 appears
 #    - How many times 10 appears (not in tuple)
+
+print("\n--- Q1: count() method ---")
+
+numbers = (1, 2, 3, 2, 4, 2, 5, 6, 2)
+
+print("Tuple:", numbers)
+print("Count of 2:", numbers.count(2))
+print("Count of 5:", numbers.count(5))
+print("Count of 10:", numbers.count(10))
+
+# ----------------------------------------------------------
