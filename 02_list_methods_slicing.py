641996bc1f44680b07fefe6eac75a32419991d9e
habiba zainab <habibazainabali@gmail.com>
2026-09-26 06:40:02 -0700

    Q10 advance slicing with palindrome list checker

--- 02_list_methods_slicing.py
+++ 02_list_methods_slicing.py
@@ -264,3 +264,9 @@ reversed_list = letters[::-1]
 print("Reversed:", reversed_list)
 
 # ----------------------------------------------------------
+
+# Q10: Advanced slicing - palindrome list checker
+#     Given: list1 = [1, 2, 3, 2, 1]
+#            list2 = [1, 2, 3, 4, 5]
+#     Check if lists are palindromes by comparing with reversed version
+#     Use slicing to reverse
