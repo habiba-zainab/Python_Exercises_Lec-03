5a64bb6489b9e36a1161c8278fb82906aa365161
habiba zainab <habibazainabali@gmail.com>
2026-09-26 10:28:08 -0700

    Q1 count method in tuple

--- 04_tuple_methods_slicing.py
+++ 04_tuple_methods_slicing.py
@@ -11,3 +11,10 @@
 # ==========================================================
 #                TUPLE METHODS
 # ==========================================================
+
+# Q1:  count() method
+#    Given: numbers = (1, 2, 3, 2, 4, 2, 5, 6, 2)
+#    Count:
+#    - How many times 2 appears
+#    - How many times 5 appears
+#    - How many times 10 appears (not in tuple)
